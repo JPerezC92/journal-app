e264eed129aec95adcd6b44e55b1be9aec1d8a53
Philip Perez Castro <jperez.c92@gmail.com>
2021-07-29 23:18:59 -0500

    UPDATE- Test on authReducer

--- src/reducers/authReducer/authThunks.test.ts
+++ src/reducers/authReducer/authThunks.test.ts
@@ -1,48 +1,115 @@
+import Swal from "sweetalert2";
 import { firebase } from "../../firebase";
 import { AuthService } from "../../services";
-import { store } from "../../store/store";
+import { mockStore, storeCleanActions } from "../../test-utils/mockStore";
+import { notesActions } from "../notesReducer";
+import { uiActions } from "../uiReducer";
+import { authActions } from "./authReducer";
 import { startRegister, startLogin, startLogout } from "./authThunks";
 
 describe("Test on authReducer", () => {
-  let state = store.getState();
-  let dispatch = store.dispatch;
+  let dispatch = mockStore.dispatch;
+  let state = mockStore.getState();
 
-  afterEach(async () => {
-    await dispatch(startLogout());
+  afterEach(() => {
+    dispatch(storeCleanActions.clean());
+    jest.restoreAllMocks();
   });
 
   test("startRegister should register a new user", async () => {
+    jest.spyOn(authActions, "login");
+    jest.spyOn(uiActions, "uiStartLoading");
+    jest.spyOn(uiActions, "uiFinishLoading");
+
+    state = mockStore.getState();
+    expect(state.authReducer.user.isLoggedIn).toBeFalsy();
+
     await dispatch(
       startRegister({
         name: "testRegister",
-        email: "registerTest@testing.com",
+        email: "testregister@testing.com",
         password: "123456",
       })
     );
 
-    state = store.getState();
-
+    state = mockStore.getState();
+    expect(uiActions.uiStartLoading).toHaveBeenCalledTimes(1);
+    expect(uiActions.uiFinishLoading).toHaveBeenCalledTimes(1);
+    expect(authActions.login).toHaveBeenCalledTimes(1);
+    expect(authActions.login).toHaveBeenCalledWith({
+      displayName: "testRegister",
+      uid: expect.any(String),
+    });
     expect(state.authReducer.user.displayName).toBe("testRegister");
     expect(state.authReducer.user.isLoggedIn).toBeTruthy();
 
-    firebase.auth().currentUser?.delete();
+    await firebase.auth().currentUser?.delete();
   });
 
-  test("startLogin should authenticate a user, startLogout should log out the current user", async () => {
+  test("when startRegister fail should call uiActions.uiFinishLoading", async () => {
+    jest.spyOn(uiActions, "uiFinishLoading");
+    jest.spyOn(Swal, "fire");
+
+    state = mockStore.getState();
+    expect(state.authReducer.user.isLoggedIn).toBeFalsy();
+
+    await dispatch(
+      startRegister({
+        name: "",
+        email: "",
+        password: "",
+      })
+    );
+
+    expect(uiActions.uiFinishLoading).toHaveBeenCalledTimes(1);
+    expect(Swal.fire).toHaveBeenCalledTimes(1);
+    expect(Swal.fire).toHaveBeenCalledWith(
+      "Error",
+      expect.any(String),
+      "error"
+    );
+  });
+
+  test("startLogin should call AuthService.loginWithEmailAndPassword, uiActions", async () => {
+    jest.spyOn(uiActions, "uiStartLoading");
+    jest.spyOn(uiActions, "uiFinishLoading");
+    jest.spyOn(AuthService, "loginWithEmailAndPassword");
+
     await dispatch(
       startLogin(() =>
         AuthService.loginWithEmailAndPassword("test@testing.com", "123456")
       )
     );
 
-    state = store.getState();
+    expect(uiActions.uiStartLoading).toHaveBeenCalledTimes(1);
+    expect(uiActions.uiFinishLoading).toHaveBeenCalledTimes(1);
+    expect(AuthService.loginWithEmailAndPassword).toHaveBeenCalledTimes(1);
+    expect(AuthService.loginWithEmailAndPassword).toHaveBeenCalledWith(
+      "test@testing.com",
+      "123456"
+    );
+  });
 
-    expect(state.authReducer.user.isLoggedIn).toBe(true);
-    expect(state.authReducer.user.uid).toBe("UO0hZ06iY3V0yfn5IAQZXGPmPun1");
+  test("startLogout should log out the current user", async () => {
+    jest.spyOn(authActions, "logout");
+    jest.spyOn(firebase.auth(), "signOut");
+    jest.spyOn(notesActions, "notesLogoutCleaning");
+
+    dispatch(
+      authActions.login({
+        displayName: "TESTING",
+        uid: "JKZHKJSDFNBASJKDASdklfhakjh",
+      })
+    );
+    state = mockStore.getState();
+    expect(state.authReducer.user.isLoggedIn).toBeTruthy();
 
     await dispatch(startLogout());
-    state = store.getState();
+    state = mockStore.getState();
 
-    expect(state.authReducer.user.isLoggedIn).toBe(false);
+    expect(state.authReducer.user.isLoggedIn).toBeFalsy();
+    expect(firebase.auth().signOut).toHaveBeenCalledTimes(1);
+    expect(authActions.logout).toHaveBeenCalledTimes(1);
+    expect(notesActions.notesLogoutCleaning).toHaveBeenCalledTimes(1);
   });
 });
