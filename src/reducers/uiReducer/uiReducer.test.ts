db69e235de088bdcb9ba4c42030261926690ce71
Philip Perez Castro <jperez.c92@gmail.com>
2021-08-01 17:38:32 -0500

    UPDATE - Test on uiReducer

--- src/reducers/uiReducer/uiReducer.test.ts
+++ src/reducers/uiReducer/uiReducer.test.ts
@@ -1,29 +1,29 @@
-import { store } from "../../store/store";
+import { mockStore } from "../../test-utils/mockStore";
 import { uiActions } from "./uiReducer";
 
 describe("Test on uiReducer", () => {
   test("all actions should work", () => {
-    let state = store.getState();
-    let dispatch = store.dispatch;
+    let state = mockStore.getState();
+    let dispatch = mockStore.dispatch;
 
     // set error
     dispatch(uiActions.setError("An error ocurred"));
-    state = store.getState();
+    state = mockStore.getState();
     expect(state.uiReducer.errorMessage).toBe("An error ocurred");
 
     // remove error
     dispatch(uiActions.removeError());
-    state = store.getState();
+    state = mockStore.getState();
     expect(state.uiReducer.errorMessage).toBe(null);
 
     // loading true
     dispatch(uiActions.uiStartLoading());
-    state = store.getState();
+    state = mockStore.getState();
     expect(state.uiReducer.loading).toBe(true);
 
     // loading false
     dispatch(uiActions.uiFinishLoading());
-    state = store.getState();
+    state = mockStore.getState();
     expect(state.uiReducer.loading).toBe(false);
   });
 });
